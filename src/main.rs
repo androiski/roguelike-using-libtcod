1f6d715be159fde5d6dd4af3cd80405d95522009
Andrew <androiski@gmail.com>
2022-10-27 20:27:54 -0700

    added tiles, map type, Game struct and render fn

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,20 @@ const SCREEN_HEIGHT: i32 = 50;
 
 const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
+// size of map
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+// COLORS for BUILDING
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
 struct Tcod {
     root: Root,
     con: Offscreen,
 }
 
-//generic Object, can be reused for ANY object
-struct Object {
-    x: i32,
-    y: i32,
-    char: char,
-    color: Color,
-}
-
-impl Object {
-    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
-        Object { x, y, char, color}
-    }
-
-    //move by given
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
-    }
-
-    //set color and draw character than represents this object at its position
-    pub fn draw(&self, con: &mut dyn Console) {
-        con.set_default_foreground(self.color);
-        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
-    }
-}
-
-fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
     //if you import in a function, only this function can use it
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
@@ -63,14 +45,121 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
         Key { code: Escape, .. } => return true,
 
         //keys for movement
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
 
         _ => {}
     }
     false
+}
+
+
+// generic Object, can be reused for ANY object
+#[derive(Debug)]
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color}
+    }
+
+    //move by given
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
+    }
+
+    //set color and draw character than represents this object at its position
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+/// A tile of the map and it properties
+/// The #[derive(…​)] bit automatically implements certain behaviours (Rust calls them traits, other languages use interfaces) you list there
+/// Debug is to let us print the Tile’s contents and
+/// Clone and Copy will let us copy the values on assignment or function call instead of moving them. So they’ll behave like e.g. integers in this respect.
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+/// the map is 2D arr of tiles
+/// created shortcut so that we just write Map and not the full statement
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+/// define fn to make the map
+/// use of vec! macro :)
+fn make_map() -> Map {
+    // fill map with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // place two pillars to test the map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+// draw/render map to window
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+    // go thru all tiles, set the BG and draw
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    // draw all objects in the list
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+
+
+     // blit contents of con to root console and present it
+     blit(
+        &tcod.con,
+        (0,0),
+        (MAP_WIDTH, MAP_HEIGHT),
+        &mut tcod.root,
+        (0,0),
+        1.0,
+        1.0,
+    );
 }
 
 
@@ -85,7 +174,7 @@ fn main() {
         .title("Rust/libtcod tutorial")
         .init();
 
-    let con = Offscreen::new(SCREEN_WIDTH,SCREEN_HEIGHT);
+    let con = Offscreen::new(MAP_WIDTH,MAP_HEIGHT);
     let mut tcod = Tcod { root, con };
 
     //create player object
@@ -96,6 +185,12 @@ fn main() {
 
     //list of objects
     let mut objects = [player, npc];
+
+    // create game, render
+    let game = Game {
+        // generate (not draw) map
+        map: make_map(),
+    };
 
     /*****************************************************************************************************/
     // main game loop
@@ -108,22 +203,14 @@ fn main() {
             object.draw(&mut tcod.con);
         }
 
-        //blit contents of con to root console and present it
-        blit(
-            &tcod.con,
-            (0,0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0,0),
-            1.0,
-            1.0,
-        );
+        //render screen
+        render_all(&mut tcod, &game, &objects);
         tcod.root.flush();
 
         let player = &mut objects[0];
 
         // key handler
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(&mut tcod, &game, player);
         if exit { break ; }
     }
     // end main game Loop
